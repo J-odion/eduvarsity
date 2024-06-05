78db339ac778c8e065004197e2d57b932b35d29b
Grace Itiung <Grace.Itiung@altbank.ng>
2024-06-05 21:14:34 +0100

    new fix

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -14,10 +14,12 @@ const Dashboard = () => {
       <Sidebar />
       <div className="flex-grow">
         <Header />
-        <div className="flex w-full justify-between gap-4 flex-1 p-4">
+        <div className="flex flex-col lg:flex-row w-full justify-between gap-4 flex-1 p-4">
           <div className='flex flex-1 flex-col gap-4'>
           <ProgressOverview />
+          <div className=" hidden lg:flex">
           <OverallSummary />
+          </div>
           <ContinueLearning />
           </div>
           <div className='flex flex-0.5 flex-col gap-4'>
